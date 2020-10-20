a296066f198e67e72595098d4d2357f4afee49f3
ttks <thaethinzarkhaingsoe@techfun-myanmar.com>
2020-10-20 14:14:43 +0630

    Add dialog at Ellispe event info icon button

--- lib/Gesture/MyGesturePage.dart
+++ lib/Gesture/MyGesturePage.dart
@@ -205,9 +205,16 @@ class _MyGesturePageState extends State<MyGesturePage> {
             left: isRTLlayout ? 0 : null,
             right: isRTLlayout ? null : 0,
             bottom: 0,
-            child:  Container(
-              margin: EdgeInsets.only(right: 8),
-              child: Icon(Icons.info, color: Colors.white,size: 20,),
+            child:  InkWell(
+              onTap: (){
+                showDialog(context: context, builder: (BuildContext context) => CustomDialog());
+              },
+              child: Container(
+
+                margin: EdgeInsets.only(right: 8),
+                child:Icon(Icons.info, color: Colors.white, size: 20,),
+
+              ),
             ),
           )
 
@@ -216,4 +223,96 @@ class _MyGesturePageState extends State<MyGesturePage> {
        );
   }
 
+}
+
+
+class CustomDialog extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      child: dialogContent(context),
+    );
+  }
+
+  Widget dialogContent(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 0.0,right: 0.0),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              top: 18.0,
+            ),
+            margin: EdgeInsets.only(top: 13.0,right: 8.0),
+            decoration: BoxDecoration(
+                color: Colors.grey,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(16.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 0.0,
+                    offset: Offset(0.0, 0.0),
+                  ),
+                ]),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                SizedBox(
+                  height: 20.0,
+                ),
+                Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: new Text("There is the description of the event. \n You can learn more detail here and \n See you again tomorrow\n There is the description of the event. \n You can learn more detail here and \n See you again tomorrow", style:TextStyle(fontSize: 15.0,color: Colors.white)),
+                    )//
+                ),
+                SizedBox(height: 24.0),
+               /* InkWell(
+                  child: Container(
+                    padding: EdgeInsets.only(top: 15.0,bottom:15.0),
+                    decoration: BoxDecoration(
+                      color:Colors.white,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(16.0),
+                          bottomRight: Radius.circular(16.0)),
+                    ),
+                    child:  Text(
+                      "OK",
+                      style: TextStyle(color: Colors.blue,fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  onTap:(){
+                    Navigator.pop(context);
+                  },
+                )*/
+              ],
+            ),
+          ),
+          Positioned(
+            right: 0.0,
+            child: GestureDetector(
+              onTap: (){
+                Navigator.of(context).pop();
+              },
+              child: Align(
+                alignment: Alignment.topRight,
+                child: CircleAvatar(
+                  radius: 14.0,
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.close, color: Colors.red),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
